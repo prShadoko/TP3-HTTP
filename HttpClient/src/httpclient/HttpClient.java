b28a7b78aba04bca9e44f0223a692ebbb4a08c71
Thomas RICHARD <thomas.richard.pro@gmail.com>
2013-05-27 11:50:51 +0200

    Initialisation des projets.

---
+++ HttpClient/src/httpclient/HttpClient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package httpclient;
+
+/**
+ *
+ * @author tom
+ */
+public class HttpClient {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+}
